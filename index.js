6a03610b2c7ac16398d17147b0f91dd52bc355f8
Colin Heffernan <c.heff543@gmail.com>
2022-06-10 14:38:49 -0400

    last commit, completed the chrome ext! localStorage, chrome api, etc

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 let myLeads = []
+
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
+const tabBtn = document.getElementById('tab-btn')
 const ulEl = document.getElementById('ul-el')
+const deleteBtn = document.getElementById('delete-btn')
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
 })
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target="_blank" href="${myLeads[i]}">
-                    ${myLeads[i]}
+                <a target="_blank" href="${leads[i]}">
+                    ${leads[i]}
                 </a>
             </li>
         `
@@ -23,3 +36,17 @@ function renderLeads() {
     ulEl.innerHTML = listItems
 
 }
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
+
